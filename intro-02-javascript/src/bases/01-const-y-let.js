9b5bb879cd04dc6a13f115824e7a769d3fb258a0
armando <46728239+rmdmrndrt117@users.noreply.github.com>
2021-01-04 19:59:28 -0600

    const y let
    se usaron los const y lets

---
+++ intro-02-javascript/src/bases/01-const-y-let.js
@@ -0,0 +1,14 @@
+
+//variable y constantes
+const nombre='armando';
+let apellido="marin";
+
+let valorDado=5;
+valorDado=4;
+console.log(nombre,apellido,valorDado);
+if(true){
+    let valorDado=6;
+    const nombre='peter';
+    console.log(valorDado,nombre);
+}
+console.log(valorDado);
